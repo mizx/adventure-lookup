a1ad82e7d1c64a2a932c153bdeb80b3d2e68c04f
Cody <mizx@users.noreply.github.com>
2018-03-20 21:33:19 -0700

    added material-ui dependency, removed unused files

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import * as React from 'react';
 import './App.css';
 
-const logo = require('./logo.svg');
+import CssBaseline from 'material-ui/CssBaseline';
 
 class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <CssBaseline />
+        <h1>Hello World</h1>
       </div>
     );
   }
